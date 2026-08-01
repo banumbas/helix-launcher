f6b1bfde7c4cf86bf411be7a16abab20d794ea18
banumbas <banumbas1@gmail.com>
2026-08-01 19:32:45 +0300

    0 0 0 6

--- nix/package.nix
+++ nix/package.nix
@@ -37,7 +37,7 @@
   soundfont-path ? "${soundfont-fluid}/share/soundfonts/FluidR3_GM2-2.sf2",
 }:
 let
-  version = "0.0.0.5";
+  version = "0.0.0.6";
   pname = "space-station-14-launcher";
 in
 buildDotnetModule rec {
